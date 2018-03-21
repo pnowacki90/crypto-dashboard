885e94ec93e88b88597b70b6390447168b01838a
Piotr Nowacki <p.nowacki90@gmail.com>
2018-03-21 21:01:26 +0100

    Heroku changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-    api_connect
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
   end
 
   def about
