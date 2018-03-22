f2dbcab0c4a84ef2f3be17d89948914c23f5c260
Piotr Nowacki <p.nowacki90@gmail.com>
2018-03-22 09:45:41 +0100

    Local variable c had to be an instance variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,13 @@ class ApplicationController < ActionController::Base
   end
 
   def find_coin_data(coin_symbol1)
-    c = @coins[1]
+    @c = @coins[1]
     for coin in @coins
       if coin_symbol1 == coin["symbol"]
-        c = coin
-        @name = c["name"]
-        @rank = c["rank"]
-        @price_usd = c["price_usd"]
+        @c = coin
+        @name = @c["name"]
+        @rank = @c["rank"]
+        @price_usd = @c["price_usd"]
       end
     end
   end
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
       find_coin_data(coin_symbol2)
       if coin_symbol2 == ""
         coin_symbol2 = "You didn't write anything!"
-      elsif coin_symbol2 != c["symbol"]
+      elsif coin_symbol2 != @c["symbol"]
         coin_symbol2 = "Wrong symbol!"
       end
     end
